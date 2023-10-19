826619cf69cd438f788d7f72d8660e1dc9893bf9
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-10-19 13:02:28 -0400

    Complete 'app.js'.

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-import { transaction } from 'transaction.js';
+// DEPENDENCIES
+const express = require('express');
+const transactions = require('./controllers/transactionsController.js');
+const cors = require('cors');
 
-console.log(transaction);
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+// Turn JSON into usable JS
+app.use(express.json());
+// Enable CORS for the server
+app.use(cors());
+
+app.use('/transactions', transactions);
+
+// ROUTES
+app.get('/', (req, res) => {
+	res.send('💰 Welcome to the Budgtr App 💰');
+});
+
+// ERRORS
+app.get('*', (req, res) => {
+	res.status(404).json({ error: 'Page not found' });
+});
+
+// EXPORT
+module.exports = app;
