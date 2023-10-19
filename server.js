d5368d1536f9609679b59c5ed39d7c0e330b00d8
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-10-19 14:04:01 -0400

    Complete 'server.js'.

---
+++ server.js
@@ -0,0 +1,11 @@
+// DEPENDENCIES
+const app = require('./app.js');
+
+// CONFIGURATION
+require('dotenv').config();
+const PORT = process.env.PORT || 3333;
+
+// LISTEN
+app.listen(PORT, () => {
+	console.log(`💰 Listening on port ${PORT} 💰`);
+});
