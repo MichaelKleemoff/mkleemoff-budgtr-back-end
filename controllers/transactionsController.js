670acfb5554cd64da51a9e557aa4b05f86595fa3
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-10-20 09:41:00 -0400

    Finish back-end.

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -42,4 +42,16 @@ transactions.delete('/:arrayIndex', (req, res) => {
 	}
 });
 
+// UPDATE ROUTE
+transactions.put('/:arrayIndex', (req, res) => {
+	const { arrayIndex } = req.params;
+
+	if (transactionsData[arrayIndex]) {
+		transactionsData[arrayIndex] = req.body;
+		res.status(200).json(transactionsData[arrayIndex]);
+	} else {
+		res.redirect('/*');
+	}
+});
+
 module.exports = transactions;
